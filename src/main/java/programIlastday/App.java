c4fd291190138db94d133fdf55216e09238e7c49
Artiom U <arti.urb@gmail.com>
2020-02-08 09:48:53 +0100

    // stream

---
+++ src/main/java/programIlastday/App.java
@@ -0,0 +1,11 @@
+package programIlastday;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        Examples.loopExample();
+        Examples.streamExample();
+    }
+}
